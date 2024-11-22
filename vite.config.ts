8ac74d0a0e3b28de32393f26f6fc06e6a9f6e455
Tvinki228 <pes-log@ukr.net>
2024-11-22 14:44:18 +0100

    Fix problems

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ghPages } from "vite-plugin-gh-pages";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-    plugins: [react(), ghPages()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                { src: "404.html", dest: "." },
+                { src: "src/assets/home-logo.webp", dest: "assets" }, // Убедитесь, что 404.html есть в корне проекта
+            ],
+        }),
+        ghPages(),
+    ],
     base: "music-suggestion-app",
 });
